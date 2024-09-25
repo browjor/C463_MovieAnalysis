014d66376100d352fe85634c192b7584f3d258e0
browjor <browjor@iu.edu>
2024-09-25 17:26:02 -0400

    working on scraper

--- src/scraper.py
+++ src/scraper.py
@@ -14,61 +14,37 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 load_dotenv()
-
-masterList = [] #Array used to store many reviews, empty to a .css file when it gets to 500 length
-
-
+gecko_path = os.getenv("GECKO_PATH")
+firefox_binary = os.getenv("FIREFOX_BINARY")
 # Setup Firefox options and path to geckodriver
 options = Options()
+options.binary_location = firefox_binary
 #options.add_argument("--headless")  # Run browser in headless mode (without opening a UI)
-service = Service(os.getenv('.\geckodriver.exe'))  # Make sure this points to the location of your geckodriver
+service = Service(gecko_path)  # Make sure this points to the location of your geckodriver
 
 # Start a new Firefox browser session
 driver = webdriver.Firefox(service=service, options=options)
 
 # Step 1: Open a webpage
-driver.get("https://letterboxd.com/film/back-to-the-future/reviews/")
+driver.get("https://letterboxd.com/film/back-to-the-future/reviews/by/added-earliest/")
 time.sleep(1)
 
 #Step 2: Get text from page
 
+masterList = [] #Array used to store many reviews, empty to a .csv file when it gets to 500 length
+
+
 
 reviewList = driver.find_elements(By.CLASS_NAME, "film-detail") #Get entire review content from page
 pageReviewList = [] #Store reviews from each page here
-
 for review in reviewList:
     pageReviewList.append(review.text) #Add reviews to array
 
-for review in pageReviewList:
-    temp = []
-    if "★★★★★" in review:
-        masterList[review] = ("5")
-    elif "★★★★½" in review:
-        masterList[review] = ("4.5")
-    elif "★★★★" in review:
-        masterList[review] = ("4")
-    elif "★★★½" in review:
-        masterList[review] = ("3.5")
-    elif "★★★" in review:
-        masterList[review] = ("3")
-    elif "★★½" in review:
-        masterList[review] = ("2.5")
-    elif "★★" in review:
-        masterList[review] = ("2")
-    elif "★½" in review:
-        masterList[review] = ("1.5")
-    else:
-        masterList[review] = ("0.5")
+for string in pageReviewList:
+    new = string.split("½ " or "★ " or )
+    print(new)
 
 
 
-
-
-
-
-
-
-    print(masterList[review])
-    print("")
 
 driver.quit()
